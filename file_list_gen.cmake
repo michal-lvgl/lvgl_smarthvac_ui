4527f3b677aea12d103dc292c699f53b88268945
michau <>
2025-10-26 11:23:52 +0100

    first attempt making UI with LVGL editor

---
+++ file_list_gen.cmake
@@ -0,0 +1,7 @@
+list(
+  APPEND
+  PROJECT_SOURCES
+  ${CMAKE_CURRENT_LIST_DIR}/components/temp_display_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/screens/test_grid_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/kotlownia_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/kotlownia.c)
